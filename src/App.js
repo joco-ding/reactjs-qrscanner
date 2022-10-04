95e424b598074b258c55d753038d2753786b910f
Joko Nugroho <jocoding@gmail.com>
2022-10-05 06:44:00 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Person4, QrCodeScanner, Stop } from "@mui/icons-material";
+import { AppBar, Avatar, Box, CssBaseline, Fab, Toolbar, Typography } from "@mui/material";
+import QrScanner from "qr-scanner";
+import React, { useState } from "react";
+
+let stopScan = false
+let hasilScan = ''
 
 function App() {
+  const [btnScan, setBtnScan] = useState(true)
+
+  const scanNow = async (isScan) => {
+    setBtnScan(isScan)
+    if (isScan) stopScan = true
+    if (btnScan === false) return
+    stopScan = false
+    await new Promise(r => setTimeout(r, 100))
+    const videoElement = document.getElementById('scanView')
+    const scanner = new QrScanner(
+      videoElement,
+      result => {
+        hasilScan = result.data
+        setBtnScan(true)
+        stopScan = true
+      },
+      {
+        onDecodeError: error => {
+          console.error(error)
+        },
+        maxScansPerSecond: 1,
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+        returnDetailedScanResult: true
+      }
+    )
+    await scanner.start()
+    while (stopScan === false) await new Promise(r => setTimeout(r, 100))
+    scanner.stop()
+    scanner.destroy()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Avatar sx={{ mr: 1, bgcolor: 'secondary.main' }}>
+            <Person4 />
+          </Avatar>
+          <Typography variant="h6">
+            Halo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        {btnScan === false && <video id="scanView" style={{
+          width: '100%',
+          maxWidth: '400px',
+          height: '100%',
+          maxHeight: '400px',
+          borderStyle: 'dotted',
+        }}>
+        </video>}
+        {btnScan &&
+          <Typography variant="h6">
+            Hasil Scan:<br />{hasilScan}
+          </Typography>}
+      </Box>
+      <Fab color={btnScan ? "primary" : "secondary"} onClick={() => scanNow(!btnScan)} sx={{ position: 'absolute', bottom: 16, right: 16 }}>
+        {btnScan && <QrCodeScanner />}
+        {btnScan === false && <Stop />}
+      </Fab>
+    </React.Fragment>
   );
 }
 
